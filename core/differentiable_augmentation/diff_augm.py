804c64f77ae6876f842b060757b872933ad53c31
dennis_tran5 <tran.dennis@hotmail.com>
2023-03-16 15:44:19 +0100

    torch utils

--- core/differentiable_augmentation/diff_augm.py
+++ core/differentiable_augmentation/diff_augm.py
@@ -1,14 +1,14 @@
 from .AugmentPipe_kornia import AugmentPipe_kornia
+from .augment import AugmentPipe
 
 
 class augment_pipe():
     def __init__(self, opt):
         if opt.use_kornia_augm:
-            self.augment_func = AugmentPipe_kornia(opt.prob_augm, opt.no_masks).to(opt.device)
+            self.augment_func = AugmentPipe_kornia(
+                opt.prob_augm, opt.no_masks).to(opt.device)
         else:
-            raise NotImplementedError("Please install Differentiable Augmentation (DA) using the instructions from https://github.com/NVlabs/stylegan2-ada-pytorch")
+            self.augment_func = AugmentPipe().to(opt.device)
 
     def __call__(self, batch, real=True):
         return self.augment_func(batch)
-
-
